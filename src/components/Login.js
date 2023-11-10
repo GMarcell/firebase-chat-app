b565a845c88b1a9870e34cc1f29bfd39d4412db0
Grand Marcell <grand1310marcell@gmail.com>
2023-11-10 17:05:12 +0700

    user sync with firestore

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,23 +1,27 @@
-import { Button } from '@mui/material'
-import React from 'react'
+import { auth } from "@/utils/firebase";
+import { Button } from "@mui/material";
+import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 
 function Login() {
-  return (
-    <div className='app'>
-      <div className='login'>
-        <div className='login__background'/>
-        <div className='login__container'>
-          <img src='/logo.png' alt='logo'/>
-          <div className='login__text'>
-            <h1>Sign In to Chat App</h1>
-          </div>
-          <Button>
-            Sign In With Google
-          </Button>
-        </div>
-      </div>
-    </div>
-  )
+	const [signInWithGoogle] = useSignInWithGoogle(auth);
+
+	return (
+		<div className="app">
+			<div className="login">
+				<div className="login__background" />
+				<div className="login__container">
+					<img src="/logo.png" alt="logo" />
+					<div className="login__text">
+						<h1>Sign In to Chat App</h1>
+					</div>
+					<Button onClick={() => signInWithGoogle()}>
+						Sign In With Google
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Login
+export default Login;
